6dabfed98985f6257ff6933e3cb03cb072d99d16
SnzDev <tms.working@gmail.com>
2020-03-25 10:19:50 -0300

    Created migration create_ong

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -5,8 +5,11 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './src/database/db.sqlite',
+    },
+    migrations:{
+      directory: './src/database/migrations',
+    },
   },
 
   staging: {
