c5259b61b05de45fb4025e8bbe613103ed34ba51
SnzDev <tms.working@gmail.com>
2020-03-25 19:01:58 -0300

    Finished backend Added dependencie cors Added join function of database

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -15,8 +15,24 @@ module.exports = {
     },
 
     async index(req,res){
-        const incidents = await connection('incidents').select('*');
         
+        const { page = 1 } = req.query;
+
+        const [count] = await connection('incidents').count();
+
+        const incidents = await connection('incidents')
+        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+        .limit(5)
+        .offset((page - 1) * 5)
+        .select(
+            'incidents.*',
+            'ongs.name',
+            'ongs.email',
+            'ongs.whatsapp',
+            'ongs.city',
+            'ongs.uf');
+        
+        res.header('X-Total-Count', count['count(*)'])
         return res.json({incidents});
     },
     
