50fe5955e93261b6a4cc465891147bd6d02e6162
SnzDev <tms.working@gmail.com>
2020-03-29 17:51:30 -0300

    Created login page Edited the global style and login style

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
+import heroesImg from '../../assets/heroes.png'
+import logoImg from '../../assets/logo.svg'
 
 export default function Logon(){
     return(
-    <h1>Hello World</h1>
+    <div className="logon-container">
+        <section className="form">
+        <img src={logoImg} alt="Be The Hero" />
+
+        <form>
+            <h1>Faça seu Logon</h1>
+            <input placeholder="Sua ID"/>
+            <button className="button" type="submit">Entrar</button>
+
+            <a href="/register">
+                <FiLogIn size={15} color="#E02041" />
+                Não Tenho Cadastro
+            </a>
+        </form>
+
+        </section>
+
+        <img src={heroesImg} alt="Heroes" />
+    </div>
+    
+    
     );
 }
