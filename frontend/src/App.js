9a933692b6fd61e45d4ee8c8031c5c225e3bf212
SnzDev <tms.working@gmail.com>
2020-03-29 17:59:39 -0300

    Added dependencie react-router-dom Created some routes Created register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
 import './global.css'
-import Logon from './pages/Logon/'
+import Routes from './routes'
 
 function App() {
   
 
-
-
   return (
     <div>
-     <Logon />
+     <Routes />
     </div>
   );
 }
--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,32 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
 import heroesImg from '../../assets/heroes.png'
 import logoImg from '../../assets/logo.svg'
 
-export default function Logon(){
-    return(
-    <div className="logon-container">
-        <section className="form">
-        <img src={logoImg} alt="Be The Hero" />
+export default function Logon() {
+    return (
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero" />
 
-        <form>
-            <h1>Faça seu Logon</h1>
-            <input placeholder="Sua ID"/>
-            <button className="button" type="submit">Entrar</button>
+                <form>
+                    <h1>Faça seu Logon</h1>
+                    <input placeholder="Sua ID" />
+                    <button className="button" type="submit">Entrar</button>
 
-            <a href="/register">
-                <FiLogIn size={15} color="#E02041" />
-                Não Tenho Cadastro
-            </a>
-        </form>
+                    <Link to="/register">
+                        <FiLogIn size={15} color="#E02041" />
+                        Não Tenho Cadastro
+                    </Link>
+                </form>
 
-        </section>
+            </section>
 
-        <img src={heroesImg} alt="Heroes" />
-    </div>
-    
-    
+            <img src={heroesImg} alt="Heroes" />
+        </div>
+
+
     );
 }
