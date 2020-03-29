12bc5792dd758ca369fac6337e9239a53c4bd9d0
SnzDev <tms.working@gmail.com>
2020-03-29 16:19:29 -0300

    Added global style Organizated the folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,16 @@
-import React, { useState } from 'react';
-import Header from './Header';
+import React from 'react';
+
+import './global.css'
+import Logon from './pages/Logon/'
 
 function App() {
-  const[counter, setCounter] = useState(0);
+  
 
-  function increment(){
-    setCounter(counter + 1);
-    console.log(counter);
-  }
+
 
   return (
     <div>
-      <Header>
-        Contador: {counter}
-      </Header>
-      <button onClick={increment}>Incrementar</button>
+     <Logon />
     </div>
   );
 }
