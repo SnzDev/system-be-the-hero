8a8edba0191f58e42d2cb7d82482e440ff10bcaa
SnzDev <tms.working@gmail.com>
2020-03-30 20:45:03 -0300

    Added dependencie navigation

--- mobile/App.js
+++ mobile/App.js
@@ -3,17 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View >
       <Text>Hello World!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
